843b50c3483c2f0f3fe2e86177023b7c636a6f30
PAX-12 <joaquin.suarezvallejos@gmail.com>
2024-04-17 02:40:15 -0300

    Update Clase 3 hasta el ejercicio 7 hecho

--- Clase 3.py
+++ Clase 3.py
@@ -1,6 +1,6 @@
 # Introducción a la Algoritmia - Clase 3
     
-# if 0<calificacion>100:  ---- Estructura in-between
+# if 0<calificacion>100: ---- Estructura in-between (muy útil)
 #     print("Calificación inválida")
 
 def Ejercicio1():
@@ -24,7 +24,7 @@ def Ejercicio2():
     
 def Ejercicio3():
     print("\n| Ejercicio 3 |")
-    num_mes = int(input("Ingresá un número de mes (1 al 12): "))
+    num_mes = int(input("Ingresá un número de mes (1 - 12): "))
     while num_mes < 1 or num_mes > 12:
         num_mes = int(input("El número que ingresaste no pertenece a ningún mes, por favor, ingresá un número válido del 1 al 12: "))
     if num_mes == 1:
@@ -74,35 +74,73 @@ def Ejercicio4():
     
 def Ejercicio5():
     print("\n| Ejercicio 5 |")
+    nota_primer_parcial = int(input("Ingresá la nota de tu primer parcial (0 - 10): "))
+    while nota_primer_parcial < 0 or nota_primer_parcial > 10:
+        nota_primer_parcial = int(input("Por favor, ingresá una nota válida (0 - 10): "))
+        
+    nota_segundo_parcial = int(input("Ahora ingresá la nota de tu segundo parcial (0 - 10): "))
+    while nota_segundo_parcial < 0 or nota_segundo_parcial > 10:
+        nota_segundo_parcial = int(input("Por favor, ingresá una nota válida (0 - 10): "))
     
+    if nota_primer_parcial >= 7 and nota_segundo_parcial >= 7:
+        print("Promocionaste la materia.")
+    elif nota_primer_parcial >= 4 and nota_segundo_parcial >= 4:
+        print("Aprobaste ambos parciales.")
+    elif nota_primer_parcial < 4 or nota_segundo_parcial < 4:
+        print("Tenés que recuperar.")
+    
+    
+def Ejercicio6():
+    print("\n| Ejercicio 6 |")
+    COSTO_BASICO_LIBRO = 5000
+    costo_total_libro = 0
+    
+    print("Una editorial determina el precio de un libro según la cantidad de páginas que contiene.")
+    num_paginas = int(input("Ingresá el número de páginas de tu libro: "))
+    while num_paginas < 1:
+        num_paginas = int(input("Por favor, ingresá un número válido: "))
+    
+    if num_paginas > 600: # Encuadernación con procedimiento especial: $5000 + $32 por página + $1200 + $3360
+        costo_total_libro = COSTO_BASICO_LIBRO + (num_paginas * 32) + 1200 + 3360
+    elif num_paginas > 300: # Encuadernación en tela: $5000 + $32 por página + $1200
+        costo_total_libro = COSTO_BASICO_LIBRO + (num_paginas * 32) + 1200
+    elif num_paginas <= 300: # Encuadernación rústica: $5000 + $32 por página
+        costo_total_libro = COSTO_BASICO_LIBRO + (num_paginas * 32)
+    
+    print(f"El costo de tu libro es de ${costo_total_libro}.")
+
     
 def Ejercicio7():
     print("\n| Ejercicio 7 |")
-    try:
-        kilometros = float(input("Ingresá la cantidad de kilómetros recorridos: "))
+    print("Calculadora de precios de viajes - Fletero")
+    PRECIO_VIAJE_MINIMO = 2700
+    precio_viaje = 0
+    
+    kilometros = float(input("Ingresá la cantidad de kilómetros recorridos: "))
+    while kilometros <= 0:
+        kilometros = float(input("Por favor, ingresá una cantidad de kilómetros válida: "))
 
-        if kilometros < 0:
-            print("Error: La cantidad de kilómetros no puede ser negativa.")
-        
-        elif kilometros == 0:
-            print("Introducí una cantidad de kilómetros válida.")
+    if kilometros > 0 and kilometros < 10:
+        precio_viaje = kilometros * 400
+    elif kilometros >= 10:
+        precio_viaje = kilometros * 200
+    else:
+        print("Error no contemplado.")
 
-        else:
-            if kilometros > 0 and kilometros < 10:
-                precio = kilometros * 400
-            elif kilometros >= 10:
-                precio = kilometros * 200
-            else:
-                print("Error no contemplado.")
+    if precio_viaje < PRECIO_VIAJE_MINIMO:
+        precio_viaje = 2700
 
-            if precio < 2700:
-                precio = 2700
+    print(f"El precio del viaje es de ${precio_viaje}.")
 
-        print("El precio del viaje es de: $", precio)
-        
-    except:
-        print("Error: Ingresá un número, no letras o caracteres especiales.")
 
+def Ejercicio8():
+    print("\n| Ejercicio 8 |")
+
+def Ejercicio9():
+    print("\n| Ejercicio 9 |")
+    
+def Ejercicio10():
+    print("\n| Ejercicio 10 |")
 
 ############################################################################################################
 
@@ -116,28 +154,20 @@ while xCheck == False:
         Ejercicio2()
     elif ejercicioSeleccionado == 3:
         Ejercicio3()
-    # elif ejercicioSeleccionado == 4:
-    #     Ejercicio4()
-    # elif ejercicioSeleccionado == 5:
-    #     Ejercicio5()
-    # elif ejercicioSeleccionado == 6:
-    #     Ejercicio6()
-    # elif ejercicioSeleccionado == 7:
-    #     Ejercicio7()
-    # elif ejercicioSeleccionado == 8:
-    #     Ejercicio8()
-    # elif ejercicioSeleccionado == 9:
-    #     Ejercicio9()
-    # elif ejercicioSeleccionado == 10:
-    #     Ejercicio10()
-    # elif ejercicioSeleccionado == 11:
-    #     Ejercicio11()
-    # elif ejercicioSeleccionado == 12:
-    #     Ejercicio12()
-    # elif ejercicioSeleccionado == 13:
-    #     Ejercicio13()
-    # elif ejercicioSeleccionado == 14:
-    #     Ejercicio14()
+    elif ejercicioSeleccionado == 4:
+        Ejercicio4()
+    elif ejercicioSeleccionado == 5:
+        Ejercicio5()
+    elif ejercicioSeleccionado == 6:
+        Ejercicio6()
+    elif ejercicioSeleccionado == 7:
+        Ejercicio7()
+    elif ejercicioSeleccionado == 8:
+        Ejercicio8()
+    elif ejercicioSeleccionado == 9:
+        Ejercicio9()
+    elif ejercicioSeleccionado == 10:
+        Ejercicio10()
     elif ejercicioSeleccionado == 0:
         xCheck = True
         print("Programa finalizado. ¡Hasta luego!")
