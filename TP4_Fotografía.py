729a0995228b37127d97d377281b429de8024cfe
PAX-12 <joaquin.suarezvallejos@gmail.com>
2024-06-19 19:21:21 -0300

    TP4 push

---
+++ TP4_Fotografía.py
@@ -0,0 +1,29 @@
+# TP 4 - Fotografía
+# Materia: Introducción a la Algoritmia
+# Profesora: Julia Monasterio
+# Alumnos: Julián Curi y Joaquín Suárez Vallejos
+
+# Enunciado:
+# Una empresa de fotógrafos se dedica a la realización de todas las fotografías en diversos
+# tipos de eventos. Todos los meses, tienen que generar la facturación de los eventos del
+# mes, el cual se calcula según la cantidad de fotos y el tipo de evento y para ello cuentan
+# con la siguiente información:
+# Tipo de evento Precio por unidad
+# hasta 50 fotos
+# Precio por unidad
+# mas de 50 fotos
+# Precio por unidad
+# más de 100 fotos
+# Costo tipo de
+# evento (*)
+# CASAMIENTO $750 $650 $600 $50000
+# 15 AÑOS $850 $750 $700 $60000
+# CUMPLEAÑOS $650 $550 N/A $35000
+# BAUTISMOS $750 $650 N/A $38000
+# OTROS $1000 $900 $800 $25000
+# (*) El costo es fijo por tipo de evento, en el mismo está incluido la cantidad de
+# empleados y el tipo de equipamiento que se tienen que ocupar por cada uno.
+# Además, se sabe que, por la capacidad de la empresa, lo máximo que pueden realizar
+# por mes son hasta 80 eventos, pero el mínimo es siempre 30. También, la cantidad de
+# fotografías mínimas por evento es de 30 y como máximo son 500. El costo de cada foto
+# sacada es de $50 la unidad, igual para todos los eventos.
