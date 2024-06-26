589636ed1b0b894c7957440132169f73ba7a6c86
JulianCuri <jucuri@uade.edu.ar>
2024-06-26 19:55:48 -0300

    Update TP4_Fotografía.py
    Se agregaron comentarios

--- TP4_Fotografía.py
+++ TP4_Fotografía.py
@@ -54,17 +54,22 @@ PRECIOS_FOTOS_EVENTO = [ # Matriz: precios de fotos por tipo de evento.
     [1000, 900, 800] # OTROS
 ]
 
-eventos_del_mes = [] # Lista: eventos realizados en el mes.
+eventos_del_mes = [] # Lista: eventos realizados en el mes
+
+# Variables totales
 cantidad_eventos = 0
-facturacion_evento = 0
-costo_evento = 0
 facturacion_total = 0
 costo_total = 0
+
+# Variables por evento
+facturacion_evento = 0
+costo_evento = 0
+
 
 
 # | FUNCIONES | 
 def MenuPrincipal():
-    print("\n| MENÚ PRINCIPAL |")
+    print("\n| MENÚ PRINCIPAL |") # Mostramos el menú
     print("- Junio de 2024 -")
     print("OPCIÓN 1: Ver el total de facturación y de benificio neto, los costos y la cantidad de eventos.")
     print("OPCIÓN 2: Ver el total de facturación y de beneficio neto por tipo de evento, los costos y la cantidad de eventos.")
@@ -74,17 +79,17 @@ def MenuPrincipal():
     
     flag = False
     
-    while flag == False:
+    while flag == False: # Pedimos input para que se seleccione opción
         try:
             opcion_elegida = int(input("\n> Por favor, ingresá el número de la opción que querés seleccionar (1-5): "))
             if opcion_elegida < 1 or opcion_elegida > 5:
                 print("Respuesta inválida.")
             else:
-                flag = True # El número es válido, sale del bucle.
+                flag = True # El número es válido, sale del bucle
         except ValueError:
             print("Respuesta inválida.")
     
-    if opcion_elegida == 1:
+    if opcion_elegida == 1: # Ejecutamos la opción según el input
         Opcion1()
     elif opcion_elegida == 2:
         Opcion2()
@@ -98,7 +103,7 @@ def MenuPrincipal():
         error()
         
 
-def Opcion1():
+def Opcion1(): # Total de la facturación del mes, los costos y cuantos eventos fueron
     print("\n| OPCIÓN 1 |")
     print("- Junio de 2024 -")
     print("Total de facturación y de beneficio neto, costos y cantidad de eventos.")
@@ -111,7 +116,7 @@ def Opcion1():
     volver_al_menu_principal()
     
         
-def Opcion2():
+def Opcion2(): # Total de facturación por tipo de evento, el costo y la cantidad de eventos ordenado por facturación
     print("\n| OPCIÓN 2 |")
     print("- Junio de 2024 -")
     print("Total de facturación y de beneficio neto por tipo de evento, costos y cantidad de eventos. Orden descendente por facturación.")
@@ -120,49 +125,49 @@ def Opcion2():
     # Crear una lista con la información de cada tipo de evento
     eventos_por_tipo = []
     for i in range(len(TIPO_EVENTO)):
-        tipo_evento = TIPO_EVENTO[i]
-        cantidad_eventos_x_tipo = 0
+        tipo_evento = TIPO_EVENTO[i] # Seleccionamos tipo de evento 
+        cantidad_eventos_x_tipo = 0 # Asignamos valores a 0 por cada iteración de tipo de evento
         facturacion_x_tipo = 0
         costos_x_tipo = 0
         for evento in eventos_del_mes:
-            if evento[0] == tipo_evento:
-                cantidad_eventos_x_tipo += 1
-                facturacion_x_tipo += evento[2]
-                costos_x_tipo += evento[3]
+            if evento[0] == tipo_evento: # Verificamos que el tipo de evento sea igual
+                cantidad_eventos_x_tipo += 1 # Contador del tipo de evento
+                facturacion_x_tipo += evento[2] # Sumamos la facturación
+                costos_x_tipo += evento[3] # Sumamos el costo
         eventos_por_tipo.append([tipo_evento, cantidad_eventos_x_tipo, facturacion_x_tipo, costos_x_tipo])
         
     ordenar_eventos_por_facturacion(eventos_por_tipo)
       
     # Imprimir la información de cada tipo de evento
     contadorEvento = 1
-    for evento in eventos_por_tipo:
+    for evento in eventos_por_tipo: # Mostramos eventos ennumerados
         print(f"{contadorEvento}. Tipo de evento: {evento[0]} | Cantidad de eventos: {evento[1]} | Facturación total: ${evento[2]} | Costos totales: ${evento[3]} | Beneficio neto: ${evento[2] - evento[3]}")
         contadorEvento += 1
         
     volver_al_menu_principal()
     
 
-def Opcion3():
+def Opcion3(): # Listado completo detallado del total facturado de cada evento con su tipo, ordenado por total facturado
     print("\n| OPCIÓN 3 |")
     print("- Junio de 2024 -")
     print("Listado completo detallado del total facturado de cada evento con su tipo. Orden descendente por facturación.")
     print("")
         
     listado_todos_los_eventos = []
-    for evento in eventos_del_mes:
+    for evento in eventos_del_mes: # Asignamos todos los eventos del mes
         listado_todos_los_eventos.append(evento)
         
     ordenar_eventos_por_facturacion(listado_todos_los_eventos)
     
     contadorEvento = 1
-    for evento in listado_todos_los_eventos:        
+    for evento in listado_todos_los_eventos: # Mostramos eventos ennumerados       
         print(f"{contadorEvento}. Tipo de evento: {evento[0]} | Facturación total: ${evento[2]}")
         contadorEvento += 1
         
     volver_al_menu_principal()
 
     
-def Opcion4():
+def Opcion4(): # Seleccionar un tipo de evento y que se detallen la facturación y cantidad de fotos de cada uno de los eventos de ese tipo
     print("\n| OPCIÓN 4 |")
     print("- Junio de 2024 -")
     print("\n1: CASAMIENTO | 2: 15 AÑOS | 3: CUMPLEAÑOS | 4: BAUTISMOS | 5: OTROS")
@@ -175,10 +180,10 @@ def Opcion4():
             if num_evento < 1 or num_evento > 5:
                 print("Respuesta inválida.")
             else:
-                flag = True # El número es válido, sale del bucle.
+                flag = True # El número es válido, sale del bucle
         except ValueError:
             print("Respuesta inválida.")
-    
+    # Se selecciona el tipo de evento que elige el usuario
     if num_evento == 1:
         tipo_evento = "CASAMIENTO"
     elif num_evento == 2:
@@ -192,7 +197,7 @@ def Opcion4():
     else:
         error()
     
-    eventos_por_tipo = []
+    eventos_por_tipo = [] # Se genera una matriz según el tipo de evento seleccionado
     for evento in eventos_del_mes:
         if evento[0] == tipo_evento:
             eventos_por_tipo.append(evento)
@@ -200,7 +205,7 @@ def Opcion4():
     ordenar_eventos_por_facturacion(eventos_por_tipo)
     
     contadorEvento = 1
-    for evento in eventos_por_tipo:
+    for evento in eventos_por_tipo: # Mostramos eventos ennumerados
         print(f"{contadorEvento}. Tipo de evento: {evento[0]} | Cantidad de fotos: {evento[1]} | Facturación total: ${evento[2]}")
         contadorEvento += 1
     
@@ -221,9 +226,9 @@ def volver_al_menu_principal():
     
     while flag == False:
         volver_al_menu_principal = str(input("\n> ¿Querés volver al menú principal? (Sí: y / No: n): "))
-        volver_al_menu_principal.lower()
+        volver_al_menu_principal.lower() # Forzamos la minúscula
     
-        if volver_al_menu_principal == "y" or volver_al_menu_principal == "yes" or volver_al_menu_principal == "n" or volver_al_menu_principal == "no":
+        if volver_al_menu_principal == "y" or volver_al_menu_principal == "yes" or volver_al_menu_principal == "n" or volver_al_menu_principal == "no": # Verificamos la respuesta
             flag = True
         else:
             print("Respuesta inválida.")
@@ -238,18 +243,18 @@ def volver_al_menu_principal():
         
 def finalizarPrograma():
     print("Programa finalizado. ¡Hasta luego!")
-    exit()
+    exit() # Finaliza el programa
         
         
 def error():
     print("Ha ocurrido un error inesperado.")
-    exit()
+    exit() # Finaliza el programa
 
 
 # | GENERACIÓN DE DATOS |
 cantidad_eventos = randint(30, 80) # Cantidad de eventos generados al azar, considerando el mínimo y el máximo del enunciado.
 for evento in range(cantidad_eventos):
-    tipo_evento = TIPO_EVENTO[randint(0, 4)]
+    tipo_evento = TIPO_EVENTO[randint(0, 4)] # Se le asigna un tipo de evento.
     cantidad_fotos = randint(30, 500) # Cantidad de fotos generada al azar, considerando el mínimo y el máximo del enunciado.
     
     if cantidad_fotos <= 50: # Precio por unidad hasta 50 fotos
@@ -265,7 +270,7 @@ for evento in range(cantidad_eventos):
             facturacion_evento = cantidad_fotos * PRECIOS_FOTOS_EVENTO[4][0]
         else:
             error()
-    elif cantidad_fotos > 50 and cantidad_fotos <= 100: # Precio por unidad más de 50 fotos
+    elif cantidad_fotos > 50 and cantidad_fotos <= 100: # Precio por unidad más de 50 fotos, hasta 100 fotos
         if tipo_evento == "CASAMIENTO":
             facturacion_evento = cantidad_fotos * PRECIOS_FOTOS_EVENTO[0][1]
         elif tipo_evento == "15 AÑOS":
@@ -294,7 +299,7 @@ for evento in range(cantidad_eventos):
     else:
         error()
         
-    if tipo_evento == "CASAMIENTO":
+    if tipo_evento == "CASAMIENTO": # Asignamos el costo según el tipo
         costo_evento = COSTO_TIPO_EVENTO[0]
     elif tipo_evento == "15 AÑOS":
         costo_evento = COSTO_TIPO_EVENTO[1]
@@ -305,9 +310,9 @@ for evento in range(cantidad_eventos):
     elif tipo_evento == "OTROS":
         costo_evento = COSTO_TIPO_EVENTO[4]
         
-    costo_total += costo_evento
+    costo_total += costo_evento # Sumamos el costo de evento por cada evento
     
-    facturacion_evento += costo_evento
+    facturacion_evento += costo_evento # Agregamos el costo del evento a la facturación
     facturacion_total += facturacion_evento
     
     eventos_del_mes.append([tipo_evento, cantidad_fotos, facturacion_evento, costo_evento])
